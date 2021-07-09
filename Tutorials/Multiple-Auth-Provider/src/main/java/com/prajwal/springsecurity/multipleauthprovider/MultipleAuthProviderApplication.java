d23c348e9eeeb75a0f5506618ab7512cf542451e
prajwalfc <prajwal_k@Live.com>
2021-07-09 10:18:52 -0500

    Multiple Authentication provider.

---
+++ Tutorials/Multiple-Auth-Provider/src/main/java/com/prajwal/springsecurity/multipleauthprovider/MultipleAuthProviderApplication.java
@@ -0,0 +1,13 @@
+package com.prajwal.springsecurity.multipleauthprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleAuthProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleAuthProviderApplication.class, args);
+    }
+
+}
---
+++ Tutorials/Multiple-Auth-Provider/src/test/java/com/prajwal/springsecurity/multipleauthprovider/MultipleAuthProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prajwal.springsecurity.multipleauthprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleAuthProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
