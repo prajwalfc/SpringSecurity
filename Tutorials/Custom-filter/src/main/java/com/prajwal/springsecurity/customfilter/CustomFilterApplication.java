777762bea48bca82f11f5c8d0cd2cd6fca310a51
Prajwal Khatiwada <prajwal_k@live.com>
2021-07-08 22:55:09 -0500

    first commit

---
+++ Tutorials/Custom-filter/src/main/java/com/prajwal/springsecurity/customfilter/CustomFilterApplication.java
@@ -0,0 +1,13 @@
+package com.prajwal.springsecurity.customfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomFilterApplication.class, args);
+    }
+
+}
---
+++ Tutorials/Custom-filter/src/test/java/com/prajwal/springsecurity/customfilter/CustomFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prajwal.springsecurity.customfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
